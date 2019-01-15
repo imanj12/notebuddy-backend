b78f52cc3c7dfe370a695b4ef7d1ab58a5c676d7
imanj12 <imanj12@gmail.com>
2019-01-15 13:52:19 -0500

    added accepts nested attribtes for macro

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,6 @@ class Note < ApplicationRecord
    has_many :note_tags
    has_many :tags, through: :note_tags
 
+   # accepts_nested_attributes_for :note_tags
+
 end
--- app/models/note_tag.rb
+++ app/models/note_tag.rb
@@ -1,4 +1,6 @@
 class NoteTag < ApplicationRecord
    belongs_to :note
    belongs_to :tag
+
+   # accepts_nested_attributes_for :tag
 end
