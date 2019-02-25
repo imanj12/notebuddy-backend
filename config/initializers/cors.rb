683adea7fae2dbd2a1a3de3ecb30d88bb4d8ba23
imanj12 <imanj12@gmail.com>
2019-02-25 18:42:54 -0500

    updated CORS policy to allow all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://notebuddy-frontend.herokuapp.com'
+   #  origins 'https://notebuddy-frontend.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
